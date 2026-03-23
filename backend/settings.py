125d411dc1048624787d0bd219c520c169a3a2c2
kailas348 <kailashraina9@gmail.com>
2026-03-23 15:29:56 +0530

    completed email configurations

--- backend/settings.py
+++ backend/settings.py
@@ -274,7 +274,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 10
+EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
